4a3478277a6c4c390edc0325c6a10ac01774b9ed
Lawal Taiwo <taiwolawal360@gmail.com>
2020-03-18 18:51:22 +0100

    Button 1 working fine

--- app/src/main/java/com/example/codelabactivitiesandintent/MainActivity.java
+++ app/src/main/java/com/example/codelabactivitiesandintent/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.codelabactivitiesandintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button Bt1;
+    Button Bt2;
+    Button Bt3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Bt1 = (Button) findViewById(R.id.button1);
+        Bt2 = (Button) findViewById(R.id.button2);
+        Bt3 = (Button) findViewById(R.id.button3);
+    }
+
+
+    public void launchSecondActivity(View view) {
+        Intent secondActivity = new Intent(this, SecondActivity.class);
+        startActivity(secondActivity);
     }
 }
